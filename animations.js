1e0e48c9699d823798bb2412125b267246476e97
Joshua Milano <milanoj925@gmail.com>
2024-08-26 10:51:54 +0100

    Made some changes, added some new files, improved the code.

---
+++ animations.js
@@ -0,0 +1,28 @@
+var controller = new ScrollMagic.Controller();
+
+// Scene1
+
+var scene = new ScrollMagic.Scene({
+triggerElement: "scene1",
+duration
+})
+
+var animation = anime({
+    targets: '.div',
+    // translateX: 250,
+    duration: 1600,
+    // easing: 'spring(1, 80, 10, 0)',
+    // loop: true,
+    // autoplay: false
+    translateX: 500,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine'
+  });
+
+//  Scene1
+//  Scene2
+
+
+
+//  Scene2
--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-var animation = anime({
-  targets: '.div',
-  // translateX: 250,
-  duration: 1600,
-  // easing: 'spring(1, 80, 10, 0)',
-  // loop: true,
-  // autoplay: false
-  translateX: 500,
-  direction: 'alternate',
-  loop: true,
-  easing: 'easeInOutSine'
-});
-
 document.querySelector('.play').onclick = animation.play;
 // document.querySelector('.return').onclick = animation.reverse;
